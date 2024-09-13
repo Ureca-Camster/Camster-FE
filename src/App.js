c0cd50bc5c329661924ef71a62cac289d5ab271a
hongsi0 <mushbaur@gmail.com>
2024-09-13 13:24:14 +0900

    Modified indent setting and index.html

--- src/App.js
+++ src/App.js
@@ -10,20 +10,20 @@ import StudyRoomPage from "./component/page/StudyRoomPage";
 import CamStudyPage from "./component/page/CamStudyPage";
 
 function App() {
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route index element={MainPage} />
-				<Route path="login" element={<LoginPage />} />
-				<Route path="register" element={<RegisterPage />} />
-				<Route path="study/:studyNo" element={<StudyRoomPage />} />
-				<Route path="study/:studyNo/camstudy" element={<CamStudyPage />} />
-				<Route path="register" element={<RegisterPage />} />
-				<Route path="post-write" element={<PostWritePage />} />
-				<Route path="post/:postId" element={<PostViewPage />} />
-			</Routes>
-		</BrowserRouter>
-	);
+    return (
+        <BrowserRouter>
+            <Routes>
+            <Route index element={MainPage} />
+            <Route path="login" element={<LoginPage />} />
+            <Route path="register" element={<RegisterPage />} />
+            <Route path="study/:studyNo" element={<StudyRoomPage />} />
+            <Route path="study/:studyNo/camstudy" element={<CamStudyPage />} />
+            <Route path="register" element={<RegisterPage />} />
+            <Route path="post-write" element={<PostWritePage />} />
+            <Route path="post/:postId" element={<PostViewPage />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
