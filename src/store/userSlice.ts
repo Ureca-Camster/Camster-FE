153113ea61d5393558de8306fd776581b7cacb05
KI HYUN YOON <116802387+spin54@users.noreply.github.com>
2024-09-24 23:09:53 +0900

    Update userSlice.ts

--- src/store/userSlice.ts
+++ src/store/userSlice.ts
@@ -2,7 +2,7 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 // UserState에 member_id 추가
 interface UserState {
-    member_id: number | null;  // DB의 member_id
+    memberId: number | null;  // DB의 member_id
     nickname: string;
     email: string;
     goalTime: number;
@@ -11,7 +11,7 @@ interface UserState {
 
 // 초기 상태에 member_id 추가
 const initialState: UserState = {
-    member_id: null,  // member_id를 초기값 null로 설정
+    memberId: null,  // member_id를 초기값 null로 설정
     nickname: '',
     email: '',
     goalTime: 0,
@@ -22,14 +22,22 @@ const userSlice = createSlice({
     name: 'user',
     initialState,
     reducers: {
-        // 유저 정보 설정 함수에 member_id 포함
-        setUser: (state, action: PayloadAction<UserState>) => {
-            return { ...state, ...action.payload };
-        },
-        // 유저 정보 리셋 함수 (member_id 포함)
-        resetUser: () => initialState,
+      setUser(state, action: PayloadAction<{ memberId: number; nickname: string; email: string; goalTime: number; todayTime: number }>) {
+        state.memberId = action.payload.memberId;
+        state.nickname = action.payload.nickname;
+        state.email = action.payload.email;
+        state.goalTime = action.payload.goalTime;
+        state.todayTime = action.payload.todayTime;
+      },
+      resetUser(state) {
+        state.memberId = null;
+        state.nickname = '';
+        state.email = '';
+        state.goalTime = 0;
+        state.todayTime = 0;
+      },
     },
-});
+  });
 
 export const { setUser, resetUser } = userSlice.actions;
 export default userSlice.reducer;
