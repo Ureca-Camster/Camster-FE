3c2a705ec9998566a532eb007dca42ea0df46a15
hongsi0 <hongsizero@gmail.com>
2024-09-27 00:40:30 +0900

    Modified style : login, memberupdate, navbar

--- src/component/LoginForm.jsx
+++ src/component/LoginForm.jsx
@@ -1,113 +1,96 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { setUser } from '../store/userSlice.ts';
-import { login } from '../store/loginSlice.ts';
-import axios from 'axios';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye } from '@fortawesome/free-solid-svg-icons';
 import './Input.css';
-import { useNavigate } from 'react-router-dom';
 
-function LoginForm() {
+function MemberUpdateForm() {
     const dispatch = useDispatch();
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [showPassword, setShowPassword] = useState(false);
-    const navigate = useNavigate();
+    const user = useSelector((state) => state.user);
 
-    const loginUser = async (email, password) => {
-        try {
-            const response = await axios.post('/members/login', {
-                email,
-                memberPassword: password
-            });
-            return response.status === 200;
-        } catch (error) {
-            console.error('Login failed:', error);
-            return false;
+    const [nickname, setNickname] = useState('');
+    const [goalTime, setGoalTime] = useState('');
+    const [todayTime, setTodayTime] = useState('');
+    const [newPassword, setNewPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [passwordMatchMessage, setPasswordMatchMessage] = useState('');
+    const [showNewPassword, setShowNewPassword] = useState(false);
+    const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
+    useEffect(() => {
+        if (user) {
+            setNickname(user.nickname);
+            setGoalTime(user.goalTime);
+            setTodayTime(user.todayTime);
         }
-    };
+    }, [user]);
 
-    const fetchUserInfo = async () => {
-        try {
-            const response = await axios.get('/members');
-            return response.data;
-        } catch (error) {
-            console.error('Failed to fetch user info:', error);
-            return null;
-        }
-    };
-
-    function handleLoginButton() {
-        dispatch(login());
-        dispatch(setUser({
-            nickname: '홍길동', 
-            email: 'hong@ureca.com',
-            goalTime: 10000,
-            todayTime: 8000,
-        }));
-        navigate("/");
-    }
-    
-    const handleLogin = async (e) => {
-        e.preventDefault();
-        const loginSuccess = await loginUser(email, password);
-        if (loginSuccess) {
-            dispatch(login());
-            const userInfo = await fetchUserInfo();
-            if (userInfo) {
-                dispatch(setUser({
-                    nickname: userInfo.nickname,
-                    email: userInfo.email,
-                    goalTime: userInfo.goalTime,
-                    todayTime: userInfo.todayTime,
-                }));
+    useEffect(() => {
+        if (confirmPassword) {
+            if (newPassword === confirmPassword) {
+                setPasswordMatchMessage('비밀번호가 일치합니다.');
+            } else {
+                setPasswordMatchMessage('비밀번호가 일치하지 않습니다.');
             }
-            navigate("/");
         } else {
-            console.log('Login failed');
-            setPassword(''); // Reset password input
+            setPasswordMatchMessage('');
         }
+    }, [newPassword, confirmPassword]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        dispatch(
+            setUser({
+                nickname,
+                email: user.email,
+                goalTime,
+                todayTime,
+            })
+        );
+        setNewPassword('');
+        setConfirmPassword('');
     };
 
-    const handleMouseDown = () => setShowPassword(true);
-    const handleMouseUp = () => setShowPassword(false);
+    const handleTogglePassword = (field) => {
+        if (field === 'new') {
+            setShowNewPassword(!showNewPassword);
+        } else if (field === 'confirm') {
+            setShowConfirmPassword(!showConfirmPassword);
+        }
+    };
 
     return (
-        <div className='login-form'>
-            <form onSubmit={handleLoginButton}>
-                <div className="input-wrapper">
-                    <label>이메일</label><br />
-                    <input
-                        type='email'
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                </div>
-                <div className="input-wrapper">
-                    <label>비밀번호</label><br />
+        <div className='modify-form'>
+            <form onSubmit={handleSubmit}>
+            <div className="input-wrapper">
+                    <label>비밀번호 수정 확인</label><br />
                     <div className="password-input-wrapper">
                         <input
-                            type={showPassword ? 'text' : 'password'}
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
+                            type={showConfirmPassword ? 'text' : 'password'}
+                            value={confirmPassword}
+                            onChange={(e) => setConfirmPassword(e.target.value)}
                         />
                         <FontAwesomeIcon 
                             icon={faEye}
-                            onMouseDown={handleMouseDown}
-                            onMouseUp={handleMouseUp}
-                            onMouseLeave={handleMouseUp}
+                            onMouseDown={() => handleTogglePassword('confirm')}
+                            onMouseUp={() => handleTogglePassword('confirm')}
+                            onMouseLeave={() => setShowConfirmPassword(false)}
                             className="password-toggle"
                         />
                     </div>
+                    <div className="password-match-container">
+                        <div className={`password-match-message ${passwordMatchMessage ? 'visible' : ''} ${newPassword === confirmPassword ? 'match' : 'mismatch'}`}>
+                            {passwordMatchMessage}
+                        </div>
+                    </div>
                 </div>
-                
-                <button className='mybtn skyblue rounded' type='submit' style={{ marginLeft: '0px' }}>
-                    로그인
+                <button className='mybtn skyblue rounded' type='submit'>
+                    수정 완료
                 </button>
             </form>
         </div>
     );
 }
 
-export default LoginForm;
+export default MemberUpdateForm;
